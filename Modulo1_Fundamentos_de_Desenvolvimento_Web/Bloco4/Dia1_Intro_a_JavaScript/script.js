3f84d11e807021a38a858076cd01e483823bbed7
Rachel Tairum <rachel.tairum@gmail.com>
2021-11-05 20:10:52 -0300

    Exercicios de aula durante os videos

--- Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco4/Dia1_Intro_a_JavaScript/script.js
+++ Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco4/Dia1_Intro_a_JavaScript/script.js
@@ -1,7 +1,86 @@
+// Variáveis e Constantes
 const myName = "Rachel";
 const birthCity = "Rio de Janeiro";
 var birthYear = 1996;
 birthYear = 2030;
-birthCity = "São Paulo";
-
 console.log(myName, birthCity, birthYear);
+
+// Tipos Primitivos
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientAge);
+console.log(typeof patientId);
+
+const base = 5;
+let height = 8;
+
+const area = base * height;
+console.log(area);
+
+const perimetro = (2*base) + (2*height);
+console.log (perimetro);
+
+// if e else 
+const notaNaTrybe = 5;
+
+if (notaNaTrybe >= 80) {
+    console.log ("Parabéns, você foi aprovada(o)!")
+} else if (notaNaTrybe < 80 && notaNaTrybe >= 60) {
+    console.log ("Você está na nossa lista de espera")
+} else {
+    console.log ("Você foi reprovada(o)")
+}
+
+// Operadores Lógicos
+    // &&
+const currentHour = 15;
+let message = "";
+
+if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir"
+} else if (currentHour >= 18 && currentHour < 22){
+    message = "Rango da noite, vamos jantar :D"
+} else if (currentHour >= 14 && currentHour < 18) {
+    message = "Vamos fazer um bolo pro café da tarde?"
+} else if (currentHour >= 11 && currentHour < 14){
+    message = "Hora do almoço!!!"
+} else if (currentHour >= 4 && currentHour < 11){
+    message = "Hmmm, cheiro de café recém passado"
+}
+
+console.log (message);
+
+    // ||
+
+let weekDay = "quarta-feira";
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log ("Oba, mais um dia de aprendizado na Trybe >:D")
+} else {
+    console.log ("FINALMENTE, descanso merecido UwU")
+}
+
+    // switch e case
+
+let resultadoProcessoSeletivo = 'aprovada';
+
+switch (resultadoProcessoSeletivo){
+    case resultadoProcessoSeletivo = 'aprovada':
+        console.log ("Parabéns, você foi aprovada(o)!");
+    break;
+    case resultadoProcessoSeletivo = 'lista':
+        console.log ("Você está na nossa lista de espera");
+    break;
+    case resultadoProcessoSeletivo = 'reprovado':
+        console.log ("Você foi reprovada(o)");
+    break;
+    default:
+        console.log ('não se aplica')
+    break;
+}
