b3b568eb7690bbb7ff28f1bdd15fc4562ace1a11
Rachel Tairum <rachel.tairum@gmail.com>
2021-11-09 09:19:27 -0300

    Exercicios Bonus

--- Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco4/Dia2_Array_e_For/scriptExercicio.js
+++ Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco4/Dia2_Array_e_For/scriptExercicio.js
@@ -114,6 +114,8 @@ console.log (divisao);
 
 // Bonus
 
+/* Ordene o array numbers em ordem crescente e imprima seus valores;*/
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (index = 1; index < numbers.length; index += 1){
@@ -127,3 +129,32 @@ for (index = 1; index < numbers.length; index += 1){
 }
 
 console.log (numbers);
+
+/* Ordene o array numbers em ordem decrescente e imprima seus valores */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (index = 1; index < numbers.length; index += 1){
+    for (index2 = 0; index2 < index; index2 += 1){
+        if (numbers[index] > numbers[index2]){
+            let position = numbers[index];
+            numbers[index] = numbers[index2];
+            numbers[index2] = position;
+        }
+    }
+}
+console.log (numbers)
+
+/* Agora crie um novo array a partir do array numbers , sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push .*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let multiplicado = [];
+
+for (index = 1; index < numbers.length; index += 1){
+    if (index + 1 < numbers.length){
+        multiplicado.push (numbers[index] * numbers[index + 1]);
+    } else {
+        multiplicado.push (numbers[index] * 2)
+    }
+}
+console.log (multiplicado);
