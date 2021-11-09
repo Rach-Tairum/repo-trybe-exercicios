1168d9fc553033ee3c225e8f7a701777790286cb
Rachel Tairum <rachel.tairum@gmail.com>
2021-11-09 20:14:00 -0300

    Exercicios do Dia

--- Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco4/Dia3_Logica/script_Exercicios_Dia.js
+++ Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco4/Dia3_Logica/script_Exercicios_Dia.js
@@ -1,10 +1,55 @@
 /* 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .*/
 
 let n = 5;
-let asteristicos = [];
+let linha = '';
 
 for (i = 0; i <= n; i += 1){
-    asteristicos.push ('*' * n);
+    linha += '*';
+}
+for (i = 0; i <= n; i += 1) {
+    console.log (linha)
 }
 
-console.log (asteristicos);
+
+/* 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.*/
+
+let n = 5;
+let asteristicos = '';
+
+for (i = 0; i <= n; i += 1){
+    asteristicos += '*';
+    console.log (asteristicos);
+}
+
+/* 3- Agora inverta o lado do triângulo.*/
+
+let n = 5;
+let asteristicos = '';
+
+for (index = 0; index <= n; index += 1){
+        asteristicos += '*';
+        let espacos = '';
+        
+        for (i = n; i > 0; i -= 1){
+            let espacos = '';
+            espacos = '_ ';
+        }
+
+    console.log (espacos + asteristicos);
+}
+
+
+/* 4- Depois, faça uma pirâmide com n asteriscos de base: */
+
+let n = 5;
+let asteristicos = '';
+
+for (i = 0; i <= n; i += 1){
+
+    if (i % 2 === 0) {
+        console.log ('_')
+    } else{
+        
+    }
+    
+}
