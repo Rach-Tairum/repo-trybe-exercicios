e9c3d62a0df8aef162660f57b8b39e40d2a02a65
Rachel Tairum <rachel.tairum@gmail.com>
2021-11-09 15:14:43 -0300

    Inicio dos exercicios do Dia

---
+++ Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco4/Dia3_Logica/script_Exercicios_Dia.js
@@ -0,0 +1,10 @@
+/* 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .*/
+
+let n = 5;
+let asteristicos = [];
+
+for (i = 0; i <= n; i += 1){
+    asteristicos.push ('*' * n);
+}
+
+console.log (asteristicos);
