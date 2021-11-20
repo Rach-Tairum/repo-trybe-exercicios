6d54748816f57a91fc2446f9c0bc36d9ad45ec68
Rachel Tairum <rachel.tairum@gmail.com>
2021-11-20 13:53:23 -0300

    Exercicio Web Storage, uma parte

--- Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco5/Dia4_WebStorage/scrip2.js
+++ Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco5/Dia4_WebStorage/scrip2.js
@@ -1,3 +1,5 @@
+window.onload = function(){
+
 let fundo = document.querySelector('#fundo')
 let corTexto = document.querySelector('#corTexto')
 let tamanho = document.querySelector('#tamanhoTexto')
@@ -6,11 +8,40 @@ let tipo = document.querySelector('#tipo-de-texto')
 
 function trocaCorDeFundo (cor){
     let fundoPagina = document.getElementById('body')
-   fundoPagina.style.backgroundColor = cor
-
+    let escolha1 = fundoPagina.style.backgroundColor = cor
+    localStorage.setItem('fundo', escolha1);
    
 }
 
 fundo.addEventListener('change', function(){
     trocaCorDeFundo(fundo.value)
-})
+    
+});
+
+function trocaCorDeLetra (cor2){
+    let corLetra = document.getElementById('body')
+    let escolha2 = corLetra.style.color = cor2
+    localStorage.setItem('letra', escolha2);
+   
+}
+
+corTexto.addEventListener('change', function(){
+    trocaCorDeLetra(corTexto.value)
+    
+});
+
+function trocaTamanhoLetra (tamanho){
+    let tamanhoTexto = document.getElementsByClassName('texto')
+
+    for (let i = 0; i < tamanhoTexto.length; i += 1){
+        let escolha3 = tamanhoTexto[i].style.fontSize = tamanho + 'px'
+        localStorage.setItem('tamanhoTexto', escolha3);
+    }
+    
+}
+
+tamanho.addEventListener('change', function(){
+   trocaTamanhoLetra(tamanho.value)
+    
+});
+}
