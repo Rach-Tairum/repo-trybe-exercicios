688cc763f310796be2ab1d80c6b187b18ac5dd8a
Rachel Tairum <rachel.tairum@gmail.com>
2021-12-06 20:28:16 -0300

    Exercicios Parte 2

--- Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco7/Dia2_JS_Objects/exercicios_course.js
+++ Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco7/Dia2_JS_Objects/exercicios_course.js
@@ -56,3 +56,92 @@ const order = {
   }
   
   console.log(orderModifier(order));
+
+
+
+// Parte 2
+
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+// 1. Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
+function addTurno(lesson, chave, valor) {
+    lesson[chave] = valor 
+}
+addTurno(lesson2, 'turno', 'noite')
+console.log(lesson2);
+
+// 2. Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const listaChaves = (obj) => Object.keys(obj)
+
+console.log(listaChaves(lesson3))
+
+// 3. Crie uma função para mostrar o tamanho de um objeto
+
+const tamanhoObjeto = (lesson) => Object.keys(lesson).length
+
+console.log(tamanhoObjeto(lesson2))
+
+// 4.Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const valoresObjeto = (lesson) => Object.values(lesson)
+
+console.log(valoresObjeto(lesson1))
+
+// 5. Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 .
+
+let allLessons = {}
+
+Object.assign(allLessons, {lesson1, lesson2, lesson3})
+
+console.log(allLessons)
+
+// 6. Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+function estudantes() {
+    const aula1 = Object.values(allLessons.lesson1)
+    const aula2 = Object.values(allLessons.lesson2)
+    const aula3 = Object.values(allLessons.lesson3)
+    
+    let somaAlunos = aula1[1] + aula2[1] + aula3[1]
+
+    console.log(somaAlunos)
+
+}
+estudantes()
+
+// 7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+
+const posicao = (lesson, posicao) => Object.values(lesson)[posicao]
+
+console.log(posicao(lesson1, 0))
+
+// 8. Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
+
+function verifica(lesson, chave, valor) {
+    if (lesson[chave] === valor) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+}
+verifica(lesson3, 'turno', 'manha')
