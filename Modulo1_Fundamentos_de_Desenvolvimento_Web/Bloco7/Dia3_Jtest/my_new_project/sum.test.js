826779d2c597b7acf8294335bb8af4c5e9c98a4d
Rachel Tairum <rachel.tairum@gmail.com>
2021-12-08 01:25:08 -0300

    exercicio 1 course

--- Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco7/Dia3_Jtest/my_new_project/sum.test.js
+++ Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco7/Dia3_Jtest/my_new_project/sum.test.js
@@ -1,5 +1,26 @@
-const sum = (a, b) => a + b;
+/*A função sum(a, b) retorna a soma do parâmetro a com o b
+Teste se o retorno de sum(4, 5) é 9
+Teste se o retorno de sum(0, 0) é 0
+Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")*/
 
-test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
+const sum = require('./sum')
+
+describe('testa somas', () => {
+    // Teste se o retorno de sum(4, 5) é 9
+    it('testa se 4 + 5 = 9', () => {
+        expect(sum(4,5)).toBe(9);
+    });
+    // Teste se o retorno de sum(0, 0) é 0
+    it('testa se 0 + 0 = 0', () => {
+        expect(sum(0,0)).toBe(0);
+    });
+    // Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+    it('testa se a função lança um erro se um dos parâmetros for uma string', () => {
+        expect(() => sum(4, '5')).toThrow();
+    });
+    // Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+    it('testa mensagem de erro', () => {
+        expect(() => sum(4, '5')).toThrow('parameters must be numbers');
+    });
 });
