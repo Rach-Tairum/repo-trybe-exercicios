fcddb33d3992d1495bc634c028568241333db98f
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-04 11:06:52 -0300

    Exemplos 1, 2 e 3

---
+++ Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco8/Dia4_Reduce/Exemplos.js
@@ -0,0 +1,110 @@
+// Exemplo 1:
+
+const numbers = [50, 85, -30, 3, 15];
+
+const getBigger = (bigger, number) => ((bigger > number) ? bigger : number);
+
+const bigger = numbers.reduce(getBigger, 0);
+console.log(bigger); // 85
+
+// Exemplo 2:
+
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+// filter e reduce
+const somaPares = numbers.filter((elemento) => elemento % 2 === 0).reduce((soma, elemento) => soma + elemento)
+
+console.log(somaPares)
+
+// só reduce
+const somaPares2 = numbers.reduce ((soma, elemento) => ((elemento % 2 === 0) ? soma + elemento : soma));
+
+console.log(somaPares2)
+
+// Exemplo 3:
+
+const estudantes = [
+    {
+      nome: 'Jorge',
+      sobrenome: 'Silva',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 67 },
+        { name: 'Português', nota: 79 },
+        { name: 'Química', nota: 70 },
+        { name: 'Biologia', nota: 65 },
+      ],
+    },
+    {
+      nome: 'Mario',
+      sobrenome: 'Ferreira',
+      idade: 15,
+      turno: 'Tarde',
+      materias: [
+        { name: 'Matemática', nota: 59 },
+        { name: 'Português', nota: 80 },
+        { name: 'Química', nota: 78 },
+        { name: 'Biologia', nota: 92 },
+      ],
+    },
+    {
+      nome: 'Jorge',
+      sobrenome: 'Santos',
+      idade: 15,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 76 },
+        { name: 'Português', nota: 90 },
+        { name: 'Química', nota: 70 },
+        { name: 'Biologia', nota: 80 },
+      ],
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'Silveira',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 91 },
+        { name: 'Português', nota: 85 },
+        { name: 'Química', nota: 92 },
+        { name: 'Biologia', nota: 90 },
+      ],
+    },
+    {
+      nome: 'Natalia',
+      sobrenome: 'Castro',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 70 },
+        { name: 'Português', nota: 70 },
+        { name: 'Química', nota: 60 },
+        { name: 'Biologia', nota: 50 },
+      ],
+    },
+    {
+      nome: 'Wilson',
+      sobrenome: 'Martins',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 80 },
+        { name: 'Português', nota: 82 },
+        { name: 'Química', nota: 79 },
+        { name: 'Biologia', nota: 75 },
+      ],
+    },
+  ];
+
+const nomeMaterias = estudantes.map((elemento) => {
+    return {nome: elemento.nome, 
+            melhor_Materia: elemento.materias.reduce((maiorNota, nota) => {
+                if (maiorNota.nota > nota.nota) {
+                    return maiorNota
+                }
+                return nota
+            }, 0).name};
+})
+console.log(nomeMaterias);
