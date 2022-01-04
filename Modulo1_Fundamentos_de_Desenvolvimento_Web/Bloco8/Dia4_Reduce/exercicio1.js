48255307b426258ac4bca106a849cab6bfafe985
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-04 15:39:43 -0300

    Exercicios 1, 2 e 3

---
+++ Modulo1_Fundamentos_de_Desenvolvimento_Web/Bloco8/Dia4_Reduce/exercicio1.js
@@ -0,0 +1,14 @@
+/* Dada uma matriz, transforme em um array */
+
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+  ];
+  
+  function flatten() {
+    const arrayUnico = arrays.reduce((acumulador, elementos) => acumulador.concat(elementos), []);
+    return arrayUnico
+  }
+
+  console.log(flatten())
