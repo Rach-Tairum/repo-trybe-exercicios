81c0c7981683c482f9bad215ccf3c6893a53cfbd
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-21 15:04:19 -0300

    Iniciando estudos com react, aprendendo a usar o app

--- Modulo2_Front-end/Bloco10/Dia1/Iniciando-Teste-React/testando-meu-computador/src/App.js
+++ Modulo2_Front-end/Bloco10/Dia1/Iniciando-Teste-React/testando-meu-computador/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+const textJSX = "Hello, JSX";
+
+const element = <h1>{textJSX}</h1>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Xulabs and save to reload.
-        </p>
-        <h1>Hello, world!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      element
+
   );
 }
 
