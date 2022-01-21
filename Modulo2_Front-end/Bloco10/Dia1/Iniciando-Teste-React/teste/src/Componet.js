78bf8cc0b62c04fb3129d492403a01b025756710
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-21 18:41:52 -0300

    Exercicio React de fixação

---
+++ Modulo2_Front-end/Bloco10/Dia1/Iniciando-Teste-React/teste/src/Componet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Componet extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Rachel Cruz Carvalho Tairum</h1>
+                <p> 25 anos, carioca de nascença que entrou pra Trybe pra ter um futuro mais promissor e descobriu uma paixão por tecnologia</p>
+            </div>
+        )
+    }
+}
+export default Componet;
