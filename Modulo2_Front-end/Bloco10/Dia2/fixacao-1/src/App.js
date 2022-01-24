fb17e8891b0ed2ab5ab951844d14a2fd20616350
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-24 15:52:30 -0300

    Adaptando fixação 1 e criando exercicio do dia

--- Modulo2_Front-end/Bloco10/Dia2/fixacao-1/src/App.js
+++ Modulo2_Front-end/Bloco10/Dia2/fixacao-1/src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import Image from './Image';
 
 function App() {
+  const X = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'
   return (
     <div className="App">
       <header className="App-header">
-        <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
+        <Image source= {X} alternativeText='Cute cat staring' />
       </header>
     </div>
   );
