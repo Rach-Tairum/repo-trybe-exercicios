21ee18ce4ec89d248b09368180e135536db48263
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-26 15:50:32 -0300

    Exercicios de fixação 3 e 4 - mundando a função para dentro da classe

--- Modulo2_Front-end/Bloco11/Dia1-Estado-e-Eventos/exercicio-fixacao-1/src/App.js
+++ Modulo2_Front-end/Bloco11/Dia1-Estado-e-Eventos/exercicio-fixacao-1/src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import Botao from './botao';
 
-const playButton = () => console.log('Oi! Eu sou o Goku!')
-const playButton2 = () => console.log('You know nothing Jon Snow')
-const playButton3 = () => console.log('Volte a forma humilde que merece CARTA CLOW')
+
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.playButton = this.playButton.bind(this)
+    this.playButton2 = this.playButton2.bind(this)
+    this.playButton3 = this.playButton3.bind(this)
+  }
+
+  playButton() {
+    console.log('Oi! Eu sou o Goku!')
+  }
+
+  playButton2() {
+    console.log('You know nothing Jon Snow')
+  }
+
+  playButton3(){
+    console.log('Volte a forma humilde que merece CARTA CLOW')
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <Botao texto={playButton} nomeBotao="Dragon Ball"/>
-        <Botao texto={playButton2} nomeBotao="GOT"/>
-        <Botao texto={playButton3} nomeBotao="Sakura Card Captors"/>
+        <Botao texto={this.playButton} nomeBotao="Dragon Ball"/>
+        <Botao texto={this.playButton2} nomeBotao="GOT"/>
+        <Botao texto={this.playButton3} nomeBotao="Sakura Card Captors"/>
         </header>
       </div>
     );
