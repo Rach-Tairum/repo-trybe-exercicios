163eb802eab2dc931164d2767435c58cfa610fe9
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-27 18:10:56 -0300

    Exercicios de fixação 3, 4, 5 e 6 - Controlando todos componente do formulário

--- Modulo2_Front-end/Bloco11/Dia2-Formularios/fixacao-parte1/src/App.js
+++ Modulo2_Front-end/Bloco11/Dia2-Formularios/fixacao-parte1/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
@@ -6,34 +5,44 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      idade: 0
+      nome: '',
+      idade: 0,
+      filmeFavorito: '',
+      divertidamente: '',
+      verifica: false
     }
-    this.handleAge = this.handleAge.bind(this)
+
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  handleAge(event) {
-    this.setState({idade: event.target.value})
+  handleChange({target}) {
+    const { name } = target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    this.setState({
+      [name]: value
+    })
   }
 
   render() {
     return (
       <div className="App">
         <form>
-          <label>
-            Nome: <input name='nome' type='text'></input>
-          </label>
+          <fieldset>
+            <label>
+              Nome: <input name='nome' type='text' value={this.state.nome} onChange={this.handleChange}></input>
+            </label>
 
-          <label>
-            idade: <input name='idade' type='number' value={this.state.idade} onChange={this.handleAge}></input>
-          </label>
-          <label>
-            Qual seu filme favortito e por que?
-            <textarea name='filme-favorito'></textarea>
-          </label>
-        
+            <label>
+              idade: <input name='idade' type='number' value={this.state.idade} onChange={this.handleChange}></input>
+            </label>
+            <label>
+              Qual seu filme favortito e por que?
+              <textarea name='filmeFavorito' value={this.state.filmeFavorito} onChange={this.handleChange}></textarea>
+            </label>
+          </fieldset>
           <label>
             Escolha seu Divertidamente:
-            <select name='divertidamente'>
+            <select name='divertidamente' value={this.state.divertidamente} onChange={this.handleChange}>
               <option>Divertidamente</option>
               <option>Alegria</option>
               <option>Tristeza</option>
@@ -41,6 +50,17 @@ class App extends React.Component {
               <option>Medo</option>
               <option>Nojinho</option>
             </select>
+          </label>
+
+          <label>
+            Essas informações estão corretas?
+            <input name='verifica' type='checkbox' value={this.state.verifica} onChange={this.handleChange}></input>
+          </label>
+
+        {/* Inputs tipo aqruivo não podem ser controlados */}
+          <label> 
+            Acrescente aqui uma foto legal!
+            <input type='file' />
           </label>
         </form>
       </div>
