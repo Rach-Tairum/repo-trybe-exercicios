fbc80c090a39c42fd28e81b6776a1470de134e0a
Rachel Tairum <rachel.tairum@gmail.com>
2022-02-15 09:54:15 -0300

    Exercicios

--- Modulo2_Front-end/Bloco10/Dia2/my-pokedex/src/App.js
+++ Modulo2_Front-end/Bloco10/Dia2/my-pokedex/src/App.js
@@ -5,6 +5,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="todosPokemons">
+        <img 
+        src='https://cutewallpaper.org/21/pokedex-phone-wallpaper/Pokedex-Wallpaper-46-Group-Wallpapers.png' alt="poxedex"/>
         <Pokedex />
       </div>
     );
